3ceed773b889c20592be99bb5e6a48992d15f850
Superdose <72471357+Superdose@users.noreply.github.com>
2024-01-21 14:22:16 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Prototype for accepting git repositories and providing status information
+
+## Spring Initializr Template
+https://start.spring.io/#!type=maven-project&language=java&platformVersion=3.2.2&packaging=jar&jvmVersion=21&groupId=de.leipzig.htwk.gitrdf&artifactId=listener&name=listener&description=Archetype%20project%20for%20HTWK%20Leipzig%20-%20Project%20to%20transform%20git%20to%20RDF&packageName=de.leipzig.htwk.gitrdf.listener&dependencies=web,lombok,devtools,data-jpa,postgresql,testcontainers
---
+++ src/test/java/de/leipzig/htwk/gitrdf/listener/TestListenerApplication.java
@@ -0,0 +1,23 @@
+package de.leipzig.htwk.gitrdf.listener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestListenerApplication {
+
+	@Bean
+	@ServiceConnection
+	PostgreSQLContainer<?> postgresContainer() {
+		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.from(ListenerApplication::main).with(TestListenerApplication.class).run(args);
+	}
+
+}
