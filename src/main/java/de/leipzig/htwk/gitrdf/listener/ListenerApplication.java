425d6fe1ffc2e1197fd788d333fb4f3741e1b533
JATQK <github@juliuskonrad.de>
2024-01-30 16:31:55 +0100

    Add Global CORS Config
    Implemented global CORS settings in ListenerApplication to allow requests from 'http://localhost:3000' Frontend Application.

--- src/main/java/de/leipzig/htwk/gitrdf/listener/ListenerApplication.java
+++ src/main/java/de/leipzig/htwk/gitrdf/listener/ListenerApplication.java
@@ -2,14 +2,33 @@ package de.leipzig.htwk.gitrdf.listener;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "de.leipzig.htwk.gitrdf.listener.database.repository")
 public class ListenerApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ListenerApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**") // Apply CORS settings to all endpoints
+						.allowedOrigins("http://localhost:3000") // or use "*" for all origins
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("*")
+						.allowCredentials(true);
+			}
+		};
 	}
 
 }
