4609e9220a7e621e813f16832480500301dc493d
Superdose <72471357+Superdose@users.noreply.github.com>
2024-03-03 12:52:14 +0100

    Remove redundant code from bad request exception

--- src/main/java/de/leipzig/htwk/gitrdf/listener/api/exception/BadRequestException.java
+++ src/main/java/de/leipzig/htwk/gitrdf/listener/api/exception/BadRequestException.java
@@ -11,7 +11,7 @@ public class BadRequestException extends RuntimeException {
         String reason = "No file was given";
         String solution = "Include a zip file containing only a .git repository in the request. The file attribute of the multipart request has to be 'file'";
 
-        String message = String.format("Status: %s, Reason: %s, Solution: %s", status, reason, solution);
+        String message = getMessageFrom(status, reason, solution);
 
         return new BadRequestException(message, status, reason, solution);
     }
@@ -22,7 +22,7 @@ public class BadRequestException extends RuntimeException {
         String reason = "No file name was specified";
         String solution = "Specify a file name as part of the multipart request. The file name is to be specified as a query parameter with the title 'name'";
 
-        String message = String.format("Status: %s, Reason: %s, Solution: %s", status, reason, solution);
+        String message = getMessageFrom(status, reason, solution);
 
         return new BadRequestException(message, status, reason, solution);
     }
@@ -33,7 +33,7 @@ public class BadRequestException extends RuntimeException {
         String reason = "All repository filter options are disabled";
         String solution = "Enable at least one repository filter option";
 
-        String message = String.format("Status: %s, Reason: %s, Solution: %s", status, reason, solution);
+        String message = getMessageFrom(status, reason, solution);
 
         return new BadRequestException(message, status, reason, solution);
     }
@@ -44,7 +44,7 @@ public class BadRequestException extends RuntimeException {
         String reason = "Unsupported content type was specified";
         String solution = String.format("Supported content type is '%s'", supportedContentType);
 
-        String message = String.format("Status: %s, Reason: %s, Solution: %s", status, reason, solution);
+        String message = getMessageFrom(status, reason, solution);
 
         return new BadRequestException(message, status, reason, solution);
     }
@@ -55,7 +55,7 @@ public class BadRequestException extends RuntimeException {
         String reason = "Invalid zip file was given";
         String solution = "The zip file should only contain the .git directory and its corresponding content";
 
-        String message = String.format("Status: %s, Reason: %s, Solution: %s", status, reason, solution);
+        String message = getMessageFrom(status, reason, solution);
 
         return new BadRequestException(message, status, reason, solution);
     }
@@ -66,7 +66,7 @@ public class BadRequestException extends RuntimeException {
         String reason = String.format("Invalid id '%s' was given", idString);
         String solution = "Provide a valid id. Example id: 55";
 
-        String message = String.format("Status: %s, Reason: %s, Solution: %s", status, reason, solution);
+        String message = getMessageFrom(status, reason, solution);
 
         return new BadRequestException(message, status, reason, solution);
     }
@@ -76,7 +76,7 @@ public class BadRequestException extends RuntimeException {
         String reason = "No owner was specified";
         String solution = "Specify an owner. For example: 'dotnet' (who is the owner for example of the repo 'core')";
 
-        String message = String.format("Status: %s, Reason: %s, Solution: %s", status, reason, solution);
+        String message = getMessageFrom(status, reason, solution);
 
         return new BadRequestException(message, status, reason, solution);
     }
@@ -86,7 +86,7 @@ public class BadRequestException extends RuntimeException {
         String reason = "No repository was specified";
         String solution = "Specify a repository. For example: 'core' (the owner 'dotnet' provides for example a 'core' repository)";
 
-        String message = String.format("Status: %s, Reason: %s, Solution: %s", status, reason, solution);
+        String message = getMessageFrom(status, reason, solution);
 
         return new BadRequestException(message, status, reason, solution);
     }
@@ -97,7 +97,7 @@ public class BadRequestException extends RuntimeException {
         String reason = "Specified repository was not yet processed and therefore also doesnt contain a rdf file to download";
         String solution = "Wait until the repository was successfully processed (ie. status of repository is 'DONE')";
 
-        String message = String.format("Status: %s, Reason: %s, Solution: %s", status, reason, solution);
+        String message = getMessageFrom(status, reason, solution);
 
         return new BadRequestException(message, status, reason, solution);
     }
@@ -112,5 +112,9 @@ public class BadRequestException extends RuntimeException {
         this.status = status;
         this.reason = reason;
         this.solution = solution;
+    }
+
+    private static String getMessageFrom(String status, String reason, String solution) {
+        return String.format("Status: %s, Reason: %s, Solution: %s", status, reason, solution);
     }
 }
