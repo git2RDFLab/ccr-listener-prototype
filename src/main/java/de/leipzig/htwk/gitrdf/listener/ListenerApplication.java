8e929bd2a028911ae0e226bf5f1489146b393a0c
Superdose <72471357+Superdose@users.noreply.github.com>
2024-03-05 16:39:06 +0100

    Update cors settings

--- src/main/java/de/leipzig/htwk/gitrdf/listener/ListenerApplication.java
+++ src/main/java/de/leipzig/htwk/gitrdf/listener/ListenerApplication.java
@@ -3,9 +3,6 @@ package de.leipzig.htwk.gitrdf.listener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
@@ -17,20 +14,6 @@ public class ListenerApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(ListenerApplication.class, args);
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**") // Apply CORS settings to all endpoints
-						.allowedOrigins("http://localhost:3000") // or use "*" for all origins
-						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-						.allowedHeaders("*")
-						.allowCredentials(true);
-			}
-		};
 	}
 
 }
